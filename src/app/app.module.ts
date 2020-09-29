ab93beeb5eff0ccce4346291c08ad4c34b0b7354
elsamanang <manangelsa07@gmail.com>
2020-09-29 14:51:46 +0200

    routing de l'appli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -12,7 +13,23 @@ import { AlertComponent } from './alert/alert.component';
 import { BraceletComponent } from './bracelet/bracelet.component';
 import { ActiviteComponent } from './activite/activite.component';
 import { RapportComponent } from './rapport/rapport.component';
+import { LoginComponent } from './login/login.component';
 import { HeadComponent } from './head/head.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   declarations: [
@@ -27,10 +44,12 @@ import { HeadComponent } from './head/head.component';
     BraceletComponent,
     ActiviteComponent,
     RapportComponent,
-    HeadComponent
+    LoginComponent,
+    HeadComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
