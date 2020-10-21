d8116c2203bb34857b54a8e17760562adfe6091e
elsamanang <manangelsa07@gmail.com>
2020-10-21 09:46:07 +0200

    model enfant

---
+++ src/app/models/enfant.ts
@@ -0,0 +1,14 @@
+import { Bracelet } from './bracelet';
+
+export class Enfant {
+
+    uid: string;
+    nom: string;
+    postnom: string;
+    prenom: string;
+    pere: string;
+    mere: Date;
+    naissance: string;
+    photo: string;
+    bracelet: Bracelet
+}
