5bc2837185cba8f17713bb2f7be33eed466573a3
elsamanang <manangelsa07@gmail.com>
2020-10-21 09:45:49 +0200

    model encadreur

---
+++ src/app/models/encadreur.ts
@@ -0,0 +1,11 @@
+export class Encadreur {
+
+    uid: string;
+    nom: string;
+    prenom: string;
+    mail: string;
+    phone: string;
+    naissance: Date;
+    adresse: string;
+    photo: string;
+}
