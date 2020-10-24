5e5457d576e36ae772983415eb408e25229e7777
elsamanang <manangelsa07@gmail.com>
2020-10-24 11:56:43 +0200

    services d'authentification

--- src/app/services/authentification.service.ts
+++ src/app/services/authentification.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthentificationService {
 
-  constructor() { }
+  constructor(private firebaseAuth: AngularFireAuth) { }
+
+  AuthLogin(provider){
+    return this.firebaseAuth.signInWithPopup(provider).then((result)=>{
+      console.log('You have been successfully logged in!')
+    }).catch((error)=>{
+      console.log(error)
+    })
+  }
+
+  //Sign up in with Email & Password
+  SignUp(email : string, password : string){
+    return this.firebaseAuth.createUserWithEmailAndPassword(email, password).then((result) => {
+      console.log("reussite !!! ")
+    }).catch((error) => {
+      window.alert(error.message);
+    })
+  }
+
+  //Sign in with Email & Password
+  SignIn(email : string, password : string) {
+    return this.firebaseAuth.signInWithEmailAndPassword(email, password).then((result) => {
+        //  this.router.navigate(['<!-- enter your route name here -->']);
+      }).catch((error) => {
+        window.alert(error.message)
+      })
+  }
 }
