605ba10538deef57bcbbc11cc12253d8b2bfa124
elsamanang <manangelsa07@gmail.com>
2020-10-24 11:59:22 +0200

    controleur login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthentificationService } from '../services/authentification.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  signupForm: FormGroup;
+
+  constructor(private router: Router,
+    private serviceauth: AuthentificationService,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.signupForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern(/[0-9a-zA-Z]{6,}/)]]
+    });
+  }
+  
+  onSubmit() {
+    const email = this.signupForm.get('email').value;
+    const password = this.signupForm.get('password').value;
+    this.serviceauth.SignUp(email, password);
+    this.router.navigate(['/home']);
   }
 
 }
