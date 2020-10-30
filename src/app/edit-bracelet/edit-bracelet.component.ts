07d7fed31a25ba1c3d8433df5d4caa5d339a6129
elsamanang <manangelsa07@gmail.com>
2020-10-30 15:12:09 +0200

    finalisation de edit bracelet

--- src/app/edit-bracelet/edit-bracelet.component.ts
+++ src/app/edit-bracelet/edit-bracelet.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Bracelet } from '../models/bracelet';
+import { CrudService } from '../services/crud.service';
 
 @Component({
   selector: 'app-edit-bracelet',
@@ -7,9 +13,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditBraceletComponent implements OnInit {
 
-  constructor() { }
+  braceletForm: FormGroup;
+  uid: string;
+  bracelet: Observable<Bracelet>;
+
+  constructor(private router: Router, 
+    private formBuilder: FormBuilder,
+    private afs: AngularFirestore,
+    private route: ActivatedRoute,
+    private serviceCrud: CrudService) { }
 
   ngOnInit() {
+    this.initForm();
+    this.uid = this.route.snapshot.paramMap.get('id');
+    this.bracelet = this.serviceCrud.doc$<Bracelet>('bracelet/'+this.uid);
+  }
+
+  initForm() {
+    this.braceletForm = this.formBuilder.group({
+      bluetooth: ['', [Validators.required]],
+      wifi: ['', [Validators.required]],
+      phone: ['', [Validators.required]]
+    })
+  }
+
+  onSubmit() {
+    const bluetooth = this.braceletForm.get('bluetooth').value;
+    const wifi = this.braceletForm.get('wifi').value;
+    const phone = this.braceletForm.get('phone').value;
+    const data: Bracelet = {
+      uid: this.uid,
+      bluetooth: bluetooth,
+      wifi: wifi,
+      gps: '',
+      phone: phone
+    }
+    this.afs.doc('bracelet/'+data.uid).set(data).then((result) => {
+      this.router.navigate(['/bracelets']);
+    }).catch((error) => {
+      window.alert("echec d'ajout");
+    })
   }
 
 }
