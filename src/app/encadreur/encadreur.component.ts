f60cc2f8bdf9bb9367db8c5f8d71e621e4aa4db1
elsamanang <manangelsa07@gmail.com>
2020-10-30 16:15:40 +0200

    finalisation de encadreur

--- src/app/encadreur/encadreur.component.ts
+++ src/app/encadreur/encadreur.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestoreCollection } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Encadreur } from '../models/encadreur';
@@ -16,15 +16,19 @@ export class EncadreurComponent implements OnInit {
   encadreurs: Observable<Encadreur[]>;
 
   constructor(private serviceCrud: CrudService,
-    private router: Router) { }
+    private router: Router,
+    private afs: AngularFirestore) { }
 
   ngOnInit() {
     this.encadreurs = this.serviceCrud.colId$('encadreur');
   }
 
-  details(uid: string) {
-    this.router.navigate(['//' + uid]);
+  delete(uid){
+    this.afs.doc('encadreur/'+uid).delete();
+  }
 
+  details(uid) {
+    this.router.navigate(['/encadreur/' + uid]);
   }
 
 }
